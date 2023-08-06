edbac078ea7a365941923778163e327d29725629
Sandino Coelho <sandino.coelho@g.globo>
2023-08-06 10:56:09 -0300

    Defining basic Connections and models

---
+++ models/getAll.go
@@ -0,0 +1,25 @@
+package models
+
+import "github.com/sandinocoelho/api-rest-golang/db"
+
+func GetAll() (todo []Todo, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query(`SELECT * FROM todos`)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		var todo Todo
+		rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+		if err != nil {
+			continue
+		}
+	}
+	todos := append(todo)
+	return todos, err
+}
